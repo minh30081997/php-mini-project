181f900fc1b6780b5dbabd6c3330a9fc2ed4a0e1
Minh <minhmyn97@gmail.com>
2020-06-29 03:31:48 +0700

    Create Model Posts and Categories

---
+++ Apps/Models/Categories.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Class Categories used to define table name
+ * 
+ * @author Min <Minhmyn97@gmail.com>
+ */
+class Apps_Models_Categories extends Apps_Libs_DbConnection
+{   
+    protected $tableName = "categories";
+}
---
+++ Apps/Models/Posts.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class Posts used to define table name
+ */
+class Apps_Models_Posts extends Apps_Libs_DbConnection
+{
+    protected $tableName = "posts";
+}
